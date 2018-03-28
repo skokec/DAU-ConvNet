839f37b6bbe27422a5574f868202dd96fd318e54
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2018-03-28 13:42:03 +0000

    Fixed compile bug for RELEASE build type

--- include/dau_conv/util/common.hpp
+++ include/dau_conv/util/common.hpp
@@ -31,7 +31,7 @@
     __M_Assert(#Expr, Expr, __FILE__, __LINE__, Msg)
 
 #else
-#   define M_Assert(Expr, Msg) ;
+#   define M_Assert(Expr, Msg)
 #endif
 
 void __M_Assert(const char* expr_str, bool expr, const char* file, int line, const char* msg);
