beca10047c95df044f50e030f9245ac639925200
vitjan <vitjan.zavrtanik@gmail.com>
2018-06-11 23:09:37 +0200

    changed build help script

--- plugins/tensorflow/scripts/start_main_build.sh
+++ plugins/tensorflow/scripts/start_main_build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd ~/Documents/5.letnik/gauss_conv/new_impl/DAU-ConvNet/build
+BUILD_DIR=~/Documents/5.letnik/gauss_conv/new_impl/VitjanZ/DAU-ConvNet/build/
+if [ ! -d $BUILD_DIR ]; then
+ mkdir $BUILD_DIR
+fi
+
+cd $BUILD_DIR
 cmake ..
 make
 cp ./plugins/tensorflow/*.so ../plugins/tensorflow/bin/
