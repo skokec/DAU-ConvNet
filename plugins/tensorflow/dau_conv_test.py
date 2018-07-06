715b8aaebeafc3bdfafccf6b68a5cfda243d5626
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2018-07-06 15:32:54 +0000

    Updated python unit test to a fully working version.

--- plugins/tensorflow/dau_conv_test.py
+++ plugins/tensorflow/dau_conv_test.py
@@ -11,6 +11,7 @@ from dau_conv import DAUGridMean
 
 from scipy.ndimage.filters import gaussian_filter
 from scipy.ndimage.filters import convolve
+from scipy.ndimage.filters import correlate
 
 from tensorflow.python.ops import init_ops
 
@@ -104,14 +105,14 @@ class DAUConvTest(unittest.TestCase):
         # pre-blur the X
         for n in range(N):
             for s in range(S):
-                x_blur[n,s,:,:] = convolve(x[n,s,:,:],weights=filter,mode='constant')
+                x_blur[n,s,:,:] = correlate(x[n,s,:,:],weights=filter,mode='constant')
 
         # then offset and sum element-wise
         y = self._offset_and_sum(x_blur,w,mu1,mu2)
 
         return y
 
-    def _offset_and_dot(self, x, error, mu1, mu2):
+    def _offset_and_dot(self, x, error_, mu1, mu2, ignore_edge_gradients=True):
         S = mu1.shape[1]
         G = mu1.shape[2]
         F = mu1.shape[3]
@@ -121,11 +122,46 @@ class DAUConvTest(unittest.TestCase):
         width_out = out_shape[-1]
         height_out = out_shape[-2]
 
+        error = error_
+
+        # set right/bottom edges to zero if we should ignore them (for GPU compatability)
+        # this must be done for competability with GPU version since by one last pixel will not be accounted accurately
+        # in GPU mode
+        if ignore_edge_gradients:
+
+            disable_last_column = False
+            disable_last_row = False
+
+            if width_out >= 64:
+                disable_last_column = width_out % 64 == 0
+            elif width_out >= 32:
+                disable_last_column = width_out % 32 == 0
+            elif width_out >= 16:
+                disable_last_column = width_out % 16 == 0
+            elif width_out >= 8:
+                disable_last_column = width_out % 8 == 0
+
+            if height_out >= 64:
+                disable_last_row = height_out % 64 == 0
+            elif height_out >= 32:
+                disable_last_row = height_out % 32 == 0
+            elif height_out >= 16:
+                disable_last_row = height_out % 16 == 0
+            elif height_out >= 8:
+                disable_last_row = height_out % 8 == 0
+
+            if disable_last_column:
+                error[:,:,:,width_out-1] = 0.0
+            if disable_last_row:
+                error[:,:,height_out-1,:] = 0.0
+
+
+
         # add padding but b
         max_offset = np.max((np.max(np.abs(mu1)),
                              np.max(np.abs(mu2))))
         padding = np.int32(np.ceil(max_offset + 1))
-        x_pad = np.pad(x, pad_width=[(0,),(0,),(padding,padding+1),(padding,padding+1)], mode='constant')
+        x_pad = np.pad(x, pad_width=[(0,),(0,),(padding,),(padding,)], mode='constant')
 
         output = np.zeros(mu1.shape,dtype=np.float32)
 
@@ -188,8 +224,8 @@ class DAUConvTest(unittest.TestCase):
                                            np.swapaxes(w, 1,3),
                                            np.swapaxes(-1 * mu1, 1,3),
                                            np.swapaxes(-1 * mu2, 1,3), sigma)
-        N = error.shape[0]
-        S = error.shape[1]
+        N = x.shape[0]
+        F = x.shape[1]
 
         sigma_val = sigma[0]
 
@@ -197,34 +233,34 @@ class DAUConvTest(unittest.TestCase):
 
         # next we need to get gradients wrt w,mu1,mu2
         if True:
-            error_w_blur = np.zeros(error.shape,dtype=np.float32)
+            x_w_blur = np.zeros(x.shape,dtype=np.float32)
             # pre-blur the X
             for n in range(N):
-                for s in range(S):
-                    error_w_blur[n,s,:,:] = convolve(error[n,s,:,:],weights=deriv_w,mode='constant')
+                for f in range(F):
+                    x_w_blur[n,f,:,:] = correlate(x[n,f,:,:],weights=deriv_w,mode='constant')
 
             # then offset and sum element-wise
-            w_grad = self._offset_and_dot(x,error_w_blur,mu1,mu2)
+            w_grad = self._offset_and_dot(x_w_blur,error,mu1,mu2, ignore_edge_gradients=True)
 
         if True:
-            error_mu1_blur = np.zeros(error.shape,dtype=np.float32)
+            x_mu1_blur = np.zeros(x.shape,dtype=np.float32)
             # pre-blur the X
             for n in range(N):
-                for s in range(S):
-                    error_mu1_blur[n,s,:,:] = convolve(error[n,s,:,:],weights=deriv_mu1,mode='constant')
+                for f in range(F):
+                    x_mu1_blur[n,f,:,:] = correlate(x[n,f,:,:],weights=deriv_mu1,mode='constant')
 
             # then offset and sum element-wise
-            mu1_grad = self._offset_and_dot(x,error_mu1_blur,mu1,mu2)
+            mu1_grad = self._offset_and_dot(x_mu1_blur,error,mu1,mu2, ignore_edge_gradients=True)
 
         if True:
-            error_mu2_blur = np.zeros(error.shape,dtype=np.float32)
+            x_mu2_blur = np.zeros(x.shape,dtype=np.float32)
             # pre-blur the X
             for n in range(N):
-                for s in range(S):
-                    error_mu2_blur[n,s,:,:] = convolve(error[n,s,:,:],weights=deriv_mu2,mode='constant')
+                for f in range(F):
+                    x_mu2_blur[n,f,:,:] = correlate(x[n,f,:,:],weights=deriv_mu2,mode='constant')
 
             # then offset and sum element-wise
-            mu2_grad = self._offset_and_dot(x,error_mu2_blur,mu1,mu2)
+            mu2_grad = self._offset_and_dot(x_mu2_blur,error,mu1,mu2, ignore_edge_gradients=True)
 
         # add multiplication with weight for mean gradients
         mu1_grad = np.multiply(mu1_grad, w)
@@ -232,63 +268,81 @@ class DAUConvTest(unittest.TestCase):
 
         return (backprop_error, w_grad, mu1_grad, mu2_grad)
 
-    def _assertMatrix(self, mat, gt_mat, rel_tolerance=0.01, plot_difference=True):
+    def _assertMatrix(self, mat, gt_mat, variable_name, rel_tolerance=0.01, plot_difference=True):
         diff_abs = np.abs(mat - gt_mat)
         diff_rel = np.nan_to_num(diff_abs / np.abs(gt_mat+1e-9))
 
-        diff_idx = np.where(diff_rel > rel_tolerance)
-        if len(diff_idx[0]) > 0:
-            avg_diff = np.mean(diff_rel[diff_rel > 0.01])
+        avg_diff = np.mean(diff_rel)
+
+        #diff_idx = np.where(diff_rel > rel_tolerance)
+
+        invalid_mask = np.logical_and(diff_rel > 1e-4, diff_abs > 1e-7)
+        diff_idx = np.where(invalid_mask)
+
+        num_diff_rate = len(diff_idx[0])/float(diff_rel.size)
+
+        avg_diff = np.mean(diff_rel[invalid_mask]) if np.sum(invalid_mask) > 0 else 0
+
+        if avg_diff > rel_tolerance and num_diff_rate > 1e-2:
 
             print('tensorflow:')
             print(mat[diff_idx[0][0],diff_idx[0][1],0:5,:])
             print('numpy:')
             print(gt_mat[diff_idx[0][0],diff_idx[0][1],0:5,:])
             print(gt_mat.shape)
-            print('Avg rel-difference: %f' % avg_diff)
+            print('Avg rel-difference: %f for \'%s\' variable' % (avg_diff,variable_name))
 
             if plot_difference:
-                h = np.histogram2d(diff_idx[2], diff_idx[3], bins=diff_rel.shape[2:4], range=[[0, diff_rel.shape[2] - 1], [0, diff_rel.shape[3] - 1]])
+                plot_diff_idx = np.where(np.logical_and(invalid_mask, diff_rel > rel_tolerance))
+
+                h = np.histogram2d(plot_diff_idx[2], plot_diff_idx[3], bins=diff_rel.shape[2:4], range=[[0, diff_rel.shape[2] - 1], [0, diff_rel.shape[3] - 1]])
 
                 plt.imshow(h[0])
-                plt.title('Avg rel-difference: %f' % avg_diff)
+                plt.title('Avg rel-difference: %f for %s' % (avg_diff,variable_name))
                 plt.show(block=True)
 
-        self.assertTrue(np.all(diff_rel <= rel_tolerance))
+        self.assertTrue(avg_diff <= rel_tolerance or num_diff_rate <= 1e-2)
 
     def test_DAUConvFixed(self):
 
-        for dd in range(20):
+        for dd in range(300):
+            N = 16
+            W = 32
+            H = 32
+            input_channels = 32
+            num_output = 32
             sigma = 0.5
-            x_rand = np.random.rand(16,32,32,32)
-            #x_rand = np.ones((16,32,32,32),dtype=np.float32)
+            x_rand = np.random.rand(N,input_channels,H,W)
+            #x_rand = np.ones((16,num_output,32,32),dtype=np.float32)
 
             #nchw
             #x_rand = tf.random_normal([16,32,64,64],dtype=tf.float32)
             #x_rand_ = tf.ones([16,32,32,32],dtype=tf.float32)
             x = tf.placeholder(tf.float32, shape = x_rand.shape)
 
-            op = DAUConv(filters=64,
+            op = DAUConv(filters=num_output,
                          dau_units=(2,2),
                          max_kernel_size=9,
                          use_bias=False,
-                         weight_initializer=tf.constant_initializer(1,dtype=np.float32),
-                         #mu1_initializer=tf.random_uniform_initializer(minval=-3, maxval=3,dtype=tf.float32),
-                         #mu2_initializer=tf.random_uniform_initializer(minval=-3, maxval=3,dtype=tf.float32),
+                         weight_initializer=tf.random_normal_initializer(stddev=0.1, dtype=np.float32),
+                         mu1_initializer=tf.random_uniform_initializer(minval=-3, maxval=3,dtype=tf.float32),
+                         mu2_initializer=tf.random_uniform_initializer(minval=-3, maxval=3,dtype=tf.float32),
+                         #weight_initializer=tf.constant_initializer(1,dtype=np.float32),
                          #mu1_initializer=tf.random_uniform_initializer(minval=-2, maxval=1.8,dtype=tf.float32),
                          #mu1_initializer=DAUMeanTest(0),
                          #mu2_initializer=DAUMeanTest(0),
-                         mu1_initializer=tf.constant_initializer(-2.56,dtype=np.float32),
-                         mu2_initializer=tf.constant_initializer(-1,dtype=np.float32),
-                         sigma_initializer=tf.constant_initializer(sigma))
+                         #mu1_initializer=tf.constant_initializer(0,dtype=np.float32),
+                         #mu2_initializer=tf.constant_initializer(0,dtype=np.float32),
+                         sigma_initializer=tf.constant_initializer(sigma),
+                         unit_testing=True)
 
             result = op(x)
-            result_error = tf.ones([np.int32(x.shape[0]),64,
-                                             np.int32(x.shape[2]),
-                                             np.int32(x.shape[3])],dtype=tf.float32)
-            #result_error = tf.random_normal([np.int32(x.shape[0]),64,
+            #result_error = tf.ones([np.int32(x.shape[0]),num_output,
             #                                 np.int32(x.shape[2]),
             #                                 np.int32(x.shape[3])],dtype=tf.float32)
+            result_error = tf.random_normal([np.int32(x.shape[0]),num_output,
+                                             np.int32(x.shape[2]),
+                                             np.int32(x.shape[3])],dtype=tf.float32)
 
             var_grad = tf.gradients(result, [x, op.dau_weights, op.dau_mu1, op.dau_mu2], grad_ys=result_error)
 
@@ -297,7 +351,7 @@ class DAUConvTest(unittest.TestCase):
 
             c = tf.ConfigProto(allow_soft_placement=True,
                                log_device_placement=True)
-            c.gpu_options.visible_device_list = '3'
+            c.gpu_options.visible_device_list = '0'
             c.gpu_options.allow_growth = True
 
             with tf.Session(config=c) as s:
@@ -314,21 +368,19 @@ class DAUConvTest(unittest.TestCase):
             gt_fwd_vals = self._forward_cpu(x=x_rand, w=w, mu1=mu1, mu2=mu2,
                                             sigma=[sigma])
 
+            gt_bwd_vals = self._backward_cpu(x=x_rand, error=r_error, w=w, mu1=mu1,mu2=mu2,
+                                             sigma=[sigma])
             # interpolation in C++ code at the right edge excludes one pixel so ignore those pixels in check
             r = r[:,:,:,:-2]
+            r_grad[0] = r_grad[0][:,:,:,:-2]
             gt_fwd_vals = gt_fwd_vals[:,:,:,:-2]
+            gt_bwd_vals = (gt_bwd_vals[0][:,:,:,:-2], gt_bwd_vals[1], gt_bwd_vals[2], gt_bwd_vals[3])
 
-            #gt_bwd_vals = self._backward_cpu(x=x_rand, error=r_error, w=w, mu1=mu1,mu2=mu2,
-            #                                 sigma=[sigma])
-
-            self._assertMatrix(r, gt_fwd_vals, rel_tolerance=0.05,plot_difference=False)
-
-
-            #self.assertTrue(np.all(diff_rel <= 0.01))
-            #self.assertTrue(np.all(np.abs(r_grad[0] - gt_bwd_vals[0]) < 0.01))
-            #self.assertTrue(np.all(np.abs(r_grad[1] - gt_bwd_vals[1]) < 0.01))
-            #self.assertTrue(np.all(np.abs(r_grad[2] - gt_bwd_vals[2]) < 0.01))
-            #self.assertTrue(np.all(np.abs(r_grad[3] - gt_bwd_vals[3]) < 0.01))
+            self._assertMatrix(r, gt_fwd_vals, 'fwd_output', rel_tolerance=0.01,plot_difference=True)
+            self._assertMatrix(r_grad[0], gt_bwd_vals[0], 'bwd_error', rel_tolerance=0.01,plot_difference=True)
+            self._assertMatrix(r_grad[1], gt_bwd_vals[1], 'bwd_w_grad', rel_tolerance=0.01, plot_difference=True)
+            self._assertMatrix(r_grad[2], gt_bwd_vals[2], 'bwd_mu1_grad', rel_tolerance=0.01, plot_difference=True)
+            self._assertMatrix(r_grad[3], gt_bwd_vals[3], 'bwd_mu2_grad', rel_tolerance=0.01, plot_difference=True)
 
 
 
