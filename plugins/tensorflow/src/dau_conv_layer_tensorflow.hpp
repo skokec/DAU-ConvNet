02a97151924a0fc211bed94a723bdb506467b4f9
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2023-06-09 14:52:07 +0200

    Fixed incorrect path for tensorflow logging.h file

--- plugins/tensorflow/src/dau_conv_layer_tensorflow.hpp
+++ plugins/tensorflow/src/dau_conv_layer_tensorflow.hpp
@@ -7,7 +7,7 @@
 
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/tensor_shape.h"
-#include "tensorflow/core/platform/default/logging.h"
+#include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/framework/shape_inference.h"
 
 #include "dau_conv/base_dau_conv_layer.hpp"
